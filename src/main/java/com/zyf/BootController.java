1ae68f608d2ee165d00a0f1d4bd14ff62680f055
Yongfeng Zeng <Yongfeng Zeng>
2017-09-13 17:15:42 +0800

    Initial commit

---
+++ src/main/java/com/zyf/BootController.java
@@ -0,0 +1,15 @@
+package com.zyf;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2017/7/21.
+ */
+@RestController
+public class BootController {
+    @RequestMapping("hellow")
+    public String hellow(){
+        return "hellow22";
+    }
+}
---
+++ src/main/java/com/zyf/MybootApplication.java
@@ -0,0 +1,13 @@
+package com.zyf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybootApplication {
+
+	public static void main(String[] args) {
+		System.out.println("ceshidaima");
+		SpringApplication.run(MybootApplication.class, args);
+	}
+}
